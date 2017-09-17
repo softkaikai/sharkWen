d0549c38603717d4f494e29ddc6a0983e7061153
softkaikai <282748212@qq.com>
2017-09-17 21:24:27 +0800

    add project to remote

---
+++ src/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(123)
+
+ReactDOM.render(
+    <div>Hello boys123</div>,
+    document.getElementById("appContainer")
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var ENTRY = path.resolve(__dirname);
+var OUTPUT = path.resolve(__dirname, './output');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: ENTRY + '/src/app/app.js',
+    output: {
+        path: OUTPUT,
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ENTRY + 'node_modules',
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-2']
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: ENTRY + 'node_modules',
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor-css.min.js',
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: false,
+            }
+        }),
+        new ExtractTextPlugin('style.css')
+    ]
+
+};
